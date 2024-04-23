8efe5cc84e78d7eeaa00b4ef6d77d9c6f8206f2d
Juan Agudelo <agudelojuanpablo398@gmail.com>
2024-04-22 21:48:22 -0500

    exercise finished

--- seccion-1/manager_events.js
+++ seccion-1/manager_events.js
@@ -1,26 +1,35 @@
 const eventsArray = []
+count = 0
+while(true){
+  option = parseInt(prompt("1: Para crear un evento\n2:Para actualizar un evento\n3:Buscar un evento\n4:Para eliminar un evento\n5:Salir"));
+  if (option === 1){
+    count +=1
+    let eventName = prompt('Ingrese el nombre del evento');
+    let dateEvent = prompt('Ingrese la fecha del evento\nEjemplo: 03/12/24');
+    let inputDescription = prompt('Ingrese una descripcion sobre este evento');
 
-alert("BIENVENIDO AL SISTEMA DE GESTOR DE EVENTOS");
-let numVeces = parseInt("Ingrese el numero de eventos que desea agregar");
-let eventName = prompt('Ingrese el nombre del evento');
-let dateEvent = prompt('Ingrese la fecha del evento\nEjemplo: 03/12/24');
-let inputDescription = prompt('Ingrese una descripcion sobre este evento');
+    let datesEvent = {
+      id: count,
+      nombre: eventName,
+      fecha: dateEvent,
+      description: inputDescription
+    };
+    eventsArray.push(datesEvent);
+    console.log(eventsArray)
+    /**
+     * EL elemento forEach itera sobre tres parametros:
+     *- 1: Elemento
+     * 2: INDICE
+     * 3: array
+     */
+    eventsArray.forEach((a,b,c)=>{ 
+      if (b === c.length-1) { 
+        alert(`El nuevo evento es ${a.nombre}\nLa fecha es ${a.fecha}\nLa descripcion es ${a.description}`) }
+    })
+    
+  }
+  else if(option === 5){
+    break
+  }
+}
 
-  let datesEvent = {
-    nombre: eventName,
-    fecha: dateEvent,
-    description: inputDescription,
-  };
-eventsArray.push(datesEvent);
-
-let newDictonary = {};
-let count = 1;
-
-// Utilizar cada evento diccionario como contador 
-  // Se esta utilizando el contador entre corchetes es decir la "clave" para agregar un nuevo par clave, valor
-  // la clave es el valor actual de contador,   y el valor asociado es el evento
-eventsArray.forEach((event) =>{
-  newDictonary[count] = event;
-  count ++;
-})
-console.log(eventsArray.nombre)
--- seccion-1/name_users_genetor.js
+++ seccion-1/name_users_genetor.js
@@ -1,24 +1,28 @@
 const userList = []
 
 // let totalUsers = Number(prompt("Cuantos nombres de usuario desea almacenar?"))
-let fullName = prompt("Ingrese su nombre completo").toLowerCase()
-let methodSplit = fullName.split(" ", 2); 
-let FirstSpacePosition = fullName.indexOf(' ');
-let firstThreeLettersName = fullName.slice(0, 3)
-let firsThreeLettersLastName = fullName.slice(FirstSpacePosition + 1, FirstSpacePosition + 4);
-let userName = firstThreeLettersName + firsThreeLettersLastName;
+while (true){
+  let fullName = prompt("1:Ingrese su nombre completo").toLowerCase()
+  let methodSplit = fullName.split(" ", 2);
+  let FirstSpacePosition = fullName.indexOf(' ');
+  let firstThreeLettersName = fullName.slice(0, 3)
+  let firsThreeLettersLastName = fullName.slice(FirstSpacePosition + 1, FirstSpacePosition + 4);
+  let userName = firstThreeLettersName + T104hreeLettersLastName;
 
-let datesUser = {
-  user: userName,
-  email: userName + '@myDomain.com'
+  let datesUser = {
+    user: userName,
+    email: userName + '@myDomain.com'
+  }
+  userList.push(datesUser)
+  let verifyName = userList[0];
+  // Con el Object accedo ah las claves
+  // con el .map accedo ah los valores correspondientes de las claves
+  let value = Object.keys(verifyName).map(result => verifyName[result])
+  console.log(value)
+  
+  // if (value.includes(fullName)) {
+  //   alert('El nombre de usuario ya existe');
+
+  // []
+  
 }
-userList.push(datesUser)
-let verifyName = userList[0];
-// Con el Object accedo ah las claves
-// con el .map accedo ah los valores correspondientes de las claves
-let value = Object.keys(verifyName).map(result => verifyName[result])
-console.log(value)
-// if (value.includes(fullName)) {
-//   alert('El nombre de usuario ya existe');
-
-// []
